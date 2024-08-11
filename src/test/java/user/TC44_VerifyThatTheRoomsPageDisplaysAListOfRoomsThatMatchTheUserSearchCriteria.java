e4c8cc514b8c3c6eff8337ed3125bfe8a758f6fe
Inorichi2412 <97783828+Inorichi2412@users.noreply.github.com>
2024-08-11 21:48:10 +0700

    commit message

--- src/test/java/user/TC44_VerifyThatTheRoomsPageDisplaysAListOfRoomsThatMatchTheUserSearchCriteria.java
+++ src/test/java/user/TC44_VerifyThatTheRoomsPageDisplaysAListOfRoomsThatMatchTheUserSearchCriteria.java
@@ -40,12 +40,14 @@ public class TC44_VerifyThatTheRoomsPageDisplaysAListOfRoomsThatMatchTheUserSear
     public void VerifyThatTheRoomsPageDisplaysAListOfRoomsThatMatchTheUserSearchCriteria() {
 
         homePage.clickLogin();
+
         loginPage.login("vyvanviet","abc123");
 
         homePage.searchRoom("2024/08/09","2024/08/10","2","2");
         softAssert.assertTrue(selectRoomPage.isItemDescriptionsDisplayed(),"Don't have any available room");
 
         softAssert.assertAll();
+
 
     }
 
